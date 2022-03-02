a53d79d8f21c2a35452b94ba2ed353ae13fd3ca1
Jethro Williams <jethrowilliams@outlook.com>
2022-03-02 17:04:29 +0000

    Add ScrollingWords component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import CountUpAndDown from './components/CountUpAndDown';
 import FadeInTextLoop from './components/FadeInTextLoop';
+import ScrollingWords from './components/ScrollingWords';
 
 const App = () => {
   return (
     <Fragment>
       <FadeInTextLoop />
       <CountUpAndDown />
+      <ScrollingWords />
     </Fragment>
   );
 };
---
+++ src/components/ScrollingWords.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import { useSpring, animated } from 'react-spring';
+
+const ScrollingWords = () => {
+  const [flip, setFlip] = useState(false);
+
+  const words = [
+    'Jethro',
+    'looked',
+    'over',
+    'all',
+    'he',
+    'had',
+    'created',
+    'and',
+    'realised',
+    'it',
+    'was',
+    'good',
+  ];
+
+  const { scroll } = useSpring({
+    scroll: (words.length - 1) * 80,
+    from: { scroll: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 1000,
+    config: { tension: 75, friction: 50 },
+    onRest: () => setFlip(!flip),
+  });
+
+  return (
+    <animated.div
+      style={{
+        position: 'relative',
+        width: '200px',
+        height: 80,
+        overflow: 'auto',
+        fontSize: '4rem',
+      }}
+      scrollTop={scroll}
+    >
+      {words.map((word, i) => (
+        <div key={`${word}_${i}`} style={{ width: '100%', height: '5rem', textAlign: 'center' }}>
+          {word}
+        </div>
+      ))}
+    </animated.div>
+  );
+};
+
+export default ScrollingWords;
